1c9860817996c3c058d15d90f5d97996c0c9372e
Waysin Liu <waysin.liu@gmail.com>
2022-11-17 02:17:57 +0800

    feat: add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# install.sh
+
+# Create bash link symbolic
+ln -sf ~/.config/dotfiles/bash/.profile ~/.profile
+ln -sf ~/.config/dotfiles/bash/.bashrc ~/.bashrc
+ln -sf ~/.config/dotfiles/bash/.aliases ~/.aliases
+
